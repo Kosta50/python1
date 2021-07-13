56993ecbe31b93b4c703cce21e6100d1d8ed397f
kosta <kostatziortzis5@gmail.com>
2021-07-12 20:02:59 -0400

    Finding the sum of the numbers in the list

--- Lists.py
+++ Lists.py
@@ -13,17 +13,29 @@
 #     print("\"Hi\" is in the list")
 # else:
 #     print("\"Hi\" is not in the list")
-l=[50,120,100,60,70]
-biggest=l[0]
-for n in range(len(l)):
-    if(biggest<l[n]):
-        biggest=l[n]
-print(biggest)
+# l=[50,120,100,60,70]
+# biggest=l[0]
+# for n in range(len(l)):
+#     if(biggest<l[n]):
+#         biggest=l[n]
+# print(biggest)
 # for n in range(len(l)):
 #     # print(n)
 #     print(l[n])
-s="banana"
-# print(s[0])
-for n in range(len(s)):
-    print(s[n])
+# s="banana"
+# # print(s[0])
+# for n in range(len(s)):
+#     print(s[n])
+l=[50,120,100,60,70]
+k=[55,125,105,5,23]
 
+s=0
+for n in range(len(l)):
+    s=(l[n]+s)
+print(s)
+
+#Find the smallest number in the list
+#Take a list and reverse it (l=[70,60,100,120,50])
+#Take a list and look for duplicates in the list, and print the duplicates
+
+# print(l+k)
